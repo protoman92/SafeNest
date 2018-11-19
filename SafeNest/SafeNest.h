a7e65c422eb5c7fbbfaf3e29ba8108ceb869559b
Hai Pham <swiften.svc@gmail.com>
2018-11-19 20:11:06 +0800

    Initial Commit

---
+++ SafeNest/SafeNest.h
@@ -0,0 +1,19 @@
+//
+//  SafeNest.h
+//  SafeNest
+//
+//  Created by Hai Pham on 11/19/18.
+//  Copyright © 2018 swiften. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SafeNest.
+FOUNDATION_EXPORT double SafeNestVersionNumber;
+
+//! Project version string for SafeNest.
+FOUNDATION_EXPORT const unsigned char SafeNestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SafeNest/PublicHeader.h>
+
+
